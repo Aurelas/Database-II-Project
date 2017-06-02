d2286129b1b56432ba259c5a82deedd1ca0f897b
Mike <Mikethemantrobus@gmail.com>
2017-06-01 22:22:15 -0400

    Connect file used to connect to the xampp server.

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+define('hostname','localhost');
+define('user','root');
+define('password','');
+define('db_name','booksdb');
+
+$connect = mysqli_connect(hostname,user,password,db_name);
+
+?>
